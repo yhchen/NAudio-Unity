b23880939e4b24557b49d277834eb97ef7aadbf7
manyu <manyu@zmhy.com>
2021-04-17 16:26:00 +0800

    origin

---
+++ Packages/NAudio/Runtime/NAudio/Mp3FileReader.cs
@@ -0,0 +1,29 @@
+using System.IO;
+
+// ReSharper disable once CheckNamespace
+namespace NAudio.Wave
+{
+
+    /// <summary>
+    /// Class for reading from MP3 files
+    /// </summary>
+    public class Mp3FileReader : Mp3FileReaderBase
+    {
+        /// <summary>Supports opening a MP3 file</summary>
+        public Mp3FileReader(string mp3FileName, FrameDecompressorBuilder decompressorBuilder)
+            : base(File.OpenRead(mp3FileName), decompressorBuilder, true)
+        {
+        }
+
+        /// <summary>
+        /// Opens MP3 from a stream rather than a file
+        /// Will not dispose of this stream itself
+        /// </summary>
+        /// <param name="inputStream">The incoming stream containing MP3 data</param>
+        public Mp3FileReader(Stream inputStream, FrameDecompressorBuilder decompressorBuilder)
+            : base(inputStream, decompressorBuilder, false)
+        {
+
+        }
+    }
+}
